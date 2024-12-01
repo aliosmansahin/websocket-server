049efd49667a2eb28c01dcc74e5cf7077e583d29
aliosmansahin <aliosmsahin01@gmail.com>
2024-12-01 18:56:17 +0300

    some code changes

--- server.py
+++ server.py
@@ -1,40 +1,34 @@
-import websockets
 import asyncio
-import os
 import http
 import signal
-import socket
 
-ip = socket.gethostbyname(socket.gethostname())
-PORT = int(os.getenv("PORT", 3000))
+from websockets.asyncio.server import serve
+
 
 async def echo(websocket):
-    print(f"Yeni bağlantı kuruldu: {websocket.remote_address}")
-    try: 
-        while True:
-            message = await websocket.recv()
-            print(f"Gelen mesaj: {message}")
-            
-            await websocket.send(f"Sunucudan yanıt: {message}")
-    except websockets.exceptions.ConnectionClosed:
-        print(f"Bağlantı kapatıldı: {websocket.remote_address}")
+    async for message in websocket:
+        await websocket.send(message)
+
 
 def health_check(connection, request):
-    if(request.path == "/healthz"):
+    if request.path == "/healthz":
         return connection.respond(http.HTTPStatus.OK, "OK\n")
 
+
 async def main():
-    try:
-        loop = asyncio.get_event_loop()
-        stop = loop.create_future()
-        loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
-        async with websockets.serve(echo, "", PORT, process_request=health_check):
-            print(f"Python WebSocket Sunucusu {ip}:{PORT} adresinde çalışıyor...")
-            await stop
-    except websockets.exceptions.WebSocketException:
-        print(f"websocket hatası")
+    async with serve(
+        echo,
+        host="",
+        port=8080,
+        process_request=health_check,
+    ):
+        await stop
 
 
-print(f"Python WebSocket Sunucusu {ip}:{PORT} adresinde başlatılıyor...")
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
